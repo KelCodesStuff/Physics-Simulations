5e2a60125849183eae04d333ce9408a49271ce39
KelCodesStuff <codesmithk@gmail.com>
2025-03-13 20:43:19 -0400

    Create PhysicsSimulationsTests.swift

---
+++ Physics-SimulationsTests/PhysicsSimulationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsSimulationsTests.swift
+//  PhysicsSimulationsTests
+//
+//  Created by Kelvin Reid on 3/13/25.
+//
+
+import Testing
+@testable import PhysicsSimulations
+
+struct PhysicsSimulationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
