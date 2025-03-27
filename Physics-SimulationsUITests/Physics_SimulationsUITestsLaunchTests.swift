9e93d04a3ba864971dc87c9e664804bca35e4e99
KelCodesStuff <codesmithk@gmail.com>
2025-03-27 16:10:21 -0400

    Create Physics_SimulationsUITestsLaunchTests.swift

---
+++ Physics-SimulationsUITests/Physics_SimulationsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Physics_SimulationsUITestsLaunchTests.swift
+//  Physics-SimulationsUITests
+//
+//  Created by Kelvin Reid on 3/27/25.
+//
+
+import XCTest
+
+final class Physics_SimulationsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
